de5de5df6e2c73c60daa3db6bdd4b47c18649314
Jacob Duijzer <jacob@duijzer.com>
2024-05-02 15:52:36 +0200

    Added env vars for cosmosdb connection

--- landingzone/frontendapi/src/HopocalypseNow.FrontendApi.Api/Startup.cs
+++ landingzone/frontendapi/src/HopocalypseNow.FrontendApi.Api/Startup.cs
@@ -17,11 +17,15 @@ public class Startup : FunctionsStartup
 
     public override void Configure(IFunctionsHostBuilder builder)
     {
+        var cosmosDbConnectionString = Environment.GetEnvironmentVariable("CosmosDbConnectionString") ??
+                               throw new ArgumentNullException("Can't find variable 'CosmosDbConnectionString'.");
+        
+        var cosmosDbDatabaseName = Environment.GetEnvironmentVariable("CosmosDbDatabaseName") ??
+                               throw new ArgumentNullException("Can't find variable 'CosmosDbDatabaseName'.");
+        
         builder.Services
             .AddPooledDbContextFactory<DatabaseContext>(
-                options => options.UseCosmos(
-                    "TODO",
-                    "TODO"));
+                options => options.UseCosmos(cosmosDbConnectionString, cosmosDbDatabaseName));
 
         builder
             .AddGraphQLFunction()
